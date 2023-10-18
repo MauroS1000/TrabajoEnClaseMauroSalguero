327f8cefbadfb1160b3ef1696d22d9215688fe18
Mauro Salguero <mauro.salguero@udla.edu.ec>
2023-10-18 08:58:35 -0500

    Agregar archivos de proyecto.

---
+++ Data/TrabajoEnClaseMauroSalgueroContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TrabajoEnClaseMauroSalguero.Models;
+
+namespace TrabajoEnClaseMauroSalguero.Data
+{
+    public class TrabajoEnClaseMauroSalgueroContext : DbContext
+    {
+        public TrabajoEnClaseMauroSalgueroContext (DbContextOptions<TrabajoEnClaseMauroSalgueroContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TrabajoEnClaseMauroSalguero.Models.Burger> Burger { get; set; } = default!;
+
+        public DbSet<TrabajoEnClaseMauroSalguero.Models.Promo>? Promo { get; set; }
+    }
+}
---
+++ Models/Promo.cs
@@ -0,0 +1,11 @@
+﻿namespace TrabajoEnClaseMauroSalguero.Models
+{
+    public class Promo
+    {
+        public int PromoId { get; set; }
+        public string? PromoName { get; set;}
+        public string? PromoDescription { get; set; }
+        public int Id { get; set; }
+        public Burger? Burger { get; set; }
+    }
+}
